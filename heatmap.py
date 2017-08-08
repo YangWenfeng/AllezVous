910e49964778e7f73bb81f9f1fd57d3e8dd72546
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-08-09 00:26:02 +0800

    easy to use heatmap with an another data.js file

--- heatmap.py
+++ heatmap.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import json
 
 train_df = pd.read_csv("input/train_2016_v2.csv", parse_dates=["transactiondate"])
 prop_df = pd.read_csv("input/properties_2016.csv")
 train_df = pd.merge(train_df, prop_df, on='parcelid', how='left')
 
-data = []
+# File heatmap/data.js is like:
+"""
+var data = {
+    "select_option_name1": {"max": max_value, "data": [{"lat": x, "lng": y, "cnt": n}, ...]},
+    "select_option_name2": {"max": max_value, "data": [{"lat": x, "lng": y, "cnt": n}, ...]}, ...
+};
+"""
+
+data = {}
 for df, name in zip([train_df, prop_df], ['train', 'prop']):
     for feature in ['regionidcounty', 'regionidcity', 'regionidneighborhood', 'regionidzip']:
         # 1e6
@@ -15,8 +24,10 @@ for df, name in zip([train_df, prop_df], ['train', 'prop']):
         nbh_pdf = pd.DataFrame({'lat': lat_dict.values, 'lng': lng_dict.values, 'cnt': cnt_dict.values})
         nbh_json = nbh_pdf.to_json(orient='records')
 
-        data.append('var %s_%s_json = {max: %d, data: %s};' % (name, feature, cnt_dict.values.max(), nbh_json))
+        key = '%s_%s_json' % (name, feature)
+        val = {'max': cnt_dict.values.max(), 'data': json.loads(nbh_json)}
+        data[key] = val
 
 f = open('heatmap/data.js', 'w')
-f.write('\n'.join(data))
+f.write('var data = %s;' % json.dumps(data))
 f.close()
