47c1f29c5cad006b17a697dcb663b61c51c405bf
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-08-11 23:53:28 +0800

    object feature fullna and label encode, one hot encode, cross validation

--- xgboost_exploration.py
+++ xgboost_exploration.py
@@ -1,214 +1,113 @@
 """
-AllezVous 1.0, by Yang
-1. base zestimate_algorithm_main.py
-2. add logerror tag as feature
+exploration with XGBoost, by Yang
+Function 'load_data' is from Kaggle,
+https://www.kaggle.com/c/zillow-prize-1/discussion/37261
 """
+
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 import gc
+import os
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
+def load_data():
+    """Load dataset"""
+    train_p = 'input/train_p'
+    prop_p = 'input/prop_p'
+    sample_p = 'input/sample_p'
 
-def reduce_categs(props):
-    def reduce_one_categs(df, col, threshold, new_val):
-        value_counts = df[col].value_counts()
-        to_remove = value_counts[value_counts <= threshold].index
-        df[col].replace(to_remove, new_val, inplace=True)
-        return df
+    if os.path.exists(train_p):
+        train = pd.read_pickle(train_p)
+    else:
+        train = pd.read_csv('input/train_2016_v2.csv',
+                            parse_dates=['transactiondate'])
+        train.to_pickle(train_p)
 
-    props = reduce_one_categs(props, 'regionidcity', 5000, 0)
-    props = reduce_one_categs(props, 'regionidneighborhood', 3000, 0)
-    props = reduce_one_categs(props, 'regionidzip', 3000, 0)
+    if os.path.exists(prop_p):
+        prop = pd.read_pickle(prop_p)
+    else:
+        prop = pd.read_csv('input/properties_2016.csv')
+        print('Binding to float32')
+        for col, dtype in zip(prop.columns, prop.dtypes):
+            if dtype == np.float64:
+                prop[col] = prop[col].astype(np.float32)
+        prop.to_pickle(prop_p)
 
+    if os.path.exists(sample_p):
+        sample = pd.read_pickle(sample_p)
+    else:
+        sample = pd.read_csv('input/sample_submission.csv')
+        sample.to_pickle(sample_p)
+    return prop, train, sample
 
-def fill_missing(props):
-    props['taxdelinquencyflag'].fillna('N', inplace=True)
-    props['airconditioningtypeid'].fillna(5, inplace=True)
-    props['architecturalstyletypeid'].fillna(7, inplace=True)
-    props['basementsqft'].fillna(0, inplace=True)
-    props['bathroomcnt'].fillna(0, inplace=True)
-    props['bedroomcnt'].fillna(1, inplace=True)
-    props['buildingclasstypeid'].fillna(4, inplace=True)
-    props['buildingqualitytypeid'].fillna(7, inplace=True)
-    props['decktypeid'].fillna(0, inplace=True)
-    props['poolcnt'].fillna(0, inplace=True)
-    props['poolsizesum'].fillna(0, inplace=True)
-    props['pooltypeid10'].fillna(0, inplace=True)
-    props['pooltypeid2'].fillna(0, inplace=True)
-    props['pooltypeid7'].fillna(0, inplace=True)
-    props['regionidcity'].fillna(0, inplace=True)
-    props['regionidcounty'].fillna(0, inplace=True)
-    props['regionidneighborhood'].fillna(0, inplace=True)
-    props['regionidzip'].fillna(0, inplace=True)
-    props['taxdelinquencyyear'].fillna(0, inplace=True)
-
-    drop_cols = ['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc',
-                 'propertycountylandusecode', 'latitude', 'longitude']
-    for c, dtype in zip(props.columns, props.dtypes):
-        if c in drop_cols:
-            continue
-        if props[c].isnull().values.any():
-            props[c].fillna(props[c].median(), inplace=True)
-
-
-
-def bind_float32(props):
-    for c, dtype in zip(props.columns, props.dtypes):
-        if dtype == np.float64:
-            props[c] = props[c].astype(np.float32)
-
-def bind_obj(props):
-    for c in props.dtypes[props.dtypes == object].index.values:
-        print 'features of Object type', c
-        if c == 'taxdelinquencyflag':
-            props[c] = (props[c] == 'Y')
-        else:
-            props[c] = (props[c] == True)
-
-
-def one_hot_encoding(df, cols):
-    for col in cols:
-        # Perform encoding on training data
-        one_hot = pd.get_dummies(df[col], prefix=col)
-        df = df.drop(col, axis=1)
-        df = df.join(one_hot)
-
-
-def get_logerror_tags(train):
-    def parse_logerror_tag(v):
-        left_split, right_split = -0.35, 0.45
-        if v <= left_split:
-            return 0
-        elif v >= right_split:
-            return 1
-        else:
-            return 2
-    return np.array([parse_logerror_tag(v) for v in train['logerror']])
-
-def logerror_tag_classfication(train, props, sample):
-    print 'Merge train and properties'
-    df_train = train.merge(props, how='left', on='parcelid')
-
-    print 'Build x_train, y_train'
-    drop_cols = ['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc',
-                 'propertycountylandusecode', 'latitude', 'longitude']
-
-    x_train = df_train.drop(drop_cols, axis=1)
-    y_train = get_logerror_tags(train)
-    train_cols = x_train.columns
-
-    del df_train; gc.collect()
-
-    split = 80000
-
-    x_train, y_train, x_valid, y_valid = x_train[:split], y_train[:split], x_train[split:], y_train[split:]
-
-    print ('Building DMatrix...')
-
-    d_train = xgb.DMatrix(x_train, label=y_train)
-    d_valid = xgb.DMatrix(x_valid, label=y_valid)
-
-    del x_train, x_valid; gc.collect()
-
-    print ('Training...')
-
-    # use softmax multi-class classification
-    params = {'eta': 0.02, 'objective': 'multi:softmax', 'max_depth': 6, 'silent': 1, 'nthread': 4, 'num_class': 3}
-
-    watchlist = [(d_train, 'train'), (d_valid, 'valid')]
-    clf = xgb.train(params, d_train, 10000, watchlist, early_stopping_rounds=100, verbose_eval=10)
-
-    del d_train, d_valid; gc.collect()
-
-    print ('Building test set...')
-
-    sample['parcelid'] = sample['ParcelId']
-
-    df_test = sample.merge(props, how='left', on='parcelid')
-    x_test = df_test[train_cols]
-    print ('Predicting on test...')
-    d_test = xgb.DMatrix(x_test)
-    p_test = clf.predict(d_test)
-
-    logerror_tag_df = pd.DataFrame({'ParcelId': sample['ParcelId'], 'logerror_tag': p_test})
-    logerror_tag_df.to_csv('output/logerr_tag_classfication.csv', index=False, float_format='%.4f')
-
-    print 'logerror_tag_classfication done'
-    return logerror_tag_df
-
-def logerror_regression(train, props, sample):
-    drop_cols = ['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc',
-                 'propertycountylandusecode', 'latitude', 'longitude']
-    df_train = train.merge(props, how='left', on='parcelid')
-    x_train = df_train.drop(drop_cols, axis=1)
-    y_train = df_train['logerror'].values
-    train_cols = x_train.columns
-
-    split = 80000
-
-    x_train, y_train, x_valid, y_valid = x_train[:split], y_train[:split], x_train[split:], y_train[split:]
-
-    print ('Building DMatrix...')
-
-    d_train = xgb.DMatrix(x_train, label=y_train)
-    d_valid = xgb.DMatrix(x_valid, label=y_valid)
-
-    del x_train, x_valid; gc.collect()
-
-    print ('Training...')
-
-    params = {'eta': 0.02, 'objective': 'reg:linear', 'eval_metric': 'mae', 'max_depth': 4, 'silent': 1}
-
-    watchlist = [(d_train, 'train'), (d_valid, 'valid')]
-    clf = xgb.train(params, d_train, 10000, watchlist, early_stopping_rounds=100, verbose_eval=10)
-
-    del d_train, d_valid; gc.collect()
-
-    print ('Building test set...')
-    sample['parcelid'] = sample['ParcelId']
-    df_test = sample.merge(props, how='left', on='parcelid')
-    x_test = df_test[train_cols]
-    d_test = xgb.DMatrix(x_test)
-
-    del x_test; gc.collect()
-
-    print ('Predicting on test...')
-
-    p_test = clf.predict(d_test)
-    sub_df = sample.copy()
-    sub_df = sub_df.drop(['parcelid'], axis=1)
-
-    for c in sub_df.columns[sub_df.columns != 'ParcelId']:
-        sub_df[c] = p_test
-    print ('Writing to CSV...')
-    sub_df.to_csv('output/sample_submission_allezvous.v1.csv', index=False, float_format='%.4f')
-
-    return sub_df
 
 print 'Loading data...'
-train = pd.read_csv('input/train_2016_v2.csv')
-props = pd.read_csv('input/properties_2016.csv')
-sample = pd.read_csv('input/sample_submission.csv')
+prop, train, sample = load_data()
 
 print 'Processing properties...'
-reduce_categs(props)
-fill_missing(props)
-bind_float32(props)
-bind_obj(props)
+prop_drop_cols = ['propertyzoningdesc', 'propertycountylandusecode', 'latitude', 'longitude']
+print 'Drop properties\' columns: %s' % ','.join(prop_drop_cols)
+prop = prop.drop(prop_drop_cols, axis=1)
+
+obj_cols = prop.columns[prop.dtypes == object]
+print 'Find features with object type: %s' % ','.join(obj_cols)
+
+print 'Encoding and fill missing value on properties\' features with object type...'
+for col in obj_cols:
+    prop[col] = prop[col].fillna(-1)
+    # encode Y class values as integers
+    label_encoder = LabelEncoder()
+    label_encoder = label_encoder.fit(prop[col].values[:])
+    prop[col] = label_encoder.transform(prop[col].values[:])
 
 one_hot_encode_cols = ['airconditioningtypeid', 'architecturalstyletypeid', 'buildingclasstypeid',
                        'heatingorsystemtypeid', 'storytypeid', 'regionidcity', 'regionidcounty',
-                       'regionidneighborhood',
-                       'regionidzip']
-one_hot_encoding(props, one_hot_encode_cols)
+                       'regionidneighborhood', 'regionidzip']
+one_hot_encode_cols.extend(set(obj_cols) - set(one_hot_encode_cols))
 
-logerror_tag_df = logerror_tag_classfication(train, props, sample)
-print logerror_tag_df.head()
-#
-# logerror_tag_df = pd.read_csv('output/logerr_tag_classfication.csv')
-props['logerror_tag'] = logerror_tag_df['logerror_tag']
-one_hot_encoding(props, ['logerror_tag'])
+print 'One Hot Encoding features: %s' % ','.join(one_hot_encode_cols)
+for col in one_hot_encode_cols:
+    one_hot = pd.get_dummies(prop[col], prefix=col)
+    df = prop.drop(col, axis=1)
+    df = prop.join(one_hot)
 
-sub_df = logerror_regression(train, props, sample)
-print sub_df.head()
+
+print 'Merge train and properties on parcelid...'
+train_with_prop = train.merge(prop, how='left', on='parcelid')
+
+train_drop_cols = ['parcelid', 'logerror', 'transactiondate']
+print 'Drop train_with_prop columns: %s' % ','.join(train_drop_cols)
+x_train = train_with_prop.drop(train_drop_cols, axis=1)
+y_train = train_with_prop['logerror'].values
+d_train = xgb.DMatrix(x_train, y_train)
+
+# xgboost params
+params = {'eta': 0.02, 'objective': 'reg:linear', 'eval_metric': 'mae', 'max_depth': 4, 'silent': 1}
+estop = 100
+FOLDS = 5
+cv_res = xgb.cv(params, d_train, num_boost_round=1000, early_stopping_rounds=estop, nfold=FOLDS,
+       verbose_eval=10, show_stdv=False)
+
+# https://stackoverflow.com/questions/40500638/xgboost-cv-and-best-iteration
+best_nrounds = int((cv_res.shape[0] - estop) / (1 - 1 / FOLDS))
+num_boost_rounds = int(round(len(cv_res) * np.sqrt(FOLDS/(FOLDS-1))))
+print 'Find best_nrounds = %d, and num_boost_rounds = %d' % (best_nrounds, num_boost_rounds)
+
+model = xgb.train(params, d_train, num_boost_round=num_boost_rounds)
+
+print ('Building test set...')
+test = sample.copy()
+test['parcelid'] = test['ParcelId']
+test_with_prop = sample.merge(prop, how='left', on='parcelid')
+d_test = xgb.DMatrix(test_with_prop[x_train.columns])
+
+print ('Predicting on test...')
+
+p_test = model.predict(d_test)
+for col in sample.columns[sample.columns != 'ParcelId']:
+    test[col] = p_test
+
+print ('Writing to CSV...')
+sample.to_csv('output/xgboost_exploration.csv', index=False, float_format='%.4f')
 
