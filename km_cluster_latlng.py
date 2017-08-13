17f91495a7c3100f6ee0f558bf43a1f525b06ce3
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-08-13 13:20:18 +0800

    k-means range(25, 500, 25)

--- km_cluster_latlng.py
+++ km_cluster_latlng.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from scipy.cluster.vq import kmeans
 
-K = range(25, 50, 25)
+K = range(25, 500, 25)
 def km_cluster(K):
     # read properties data
     prop_df = pd.read_csv("input/properties_2016.csv")
